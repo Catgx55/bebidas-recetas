6377d7c38ddc270d63885d9d08bc6b2963b1b2e9
Cesar Trujillo <catgx55@gmail.com>
2024-08-18 07:05:06 -0500

    btn-cerrar y btn-favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -67,6 +67,18 @@ export default function Modal() {
                     Instrucciones
                   </Dialog.Title>
                   <p className='text-lg'>{selectedRecipe.strInstructions}</p>
+                  <div className='mt-5 flex justify-between gap-4'>
+                    <button
+                        type='button'
+                        className='w-full bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500'
+                        onClick={closedModal}
+                    >Cerrar</button>
+
+                    <button
+                        type='button'
+                        className='w-full bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
+                    >Agregar a Favoritos</button>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
